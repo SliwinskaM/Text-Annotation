b998763b639a87fbf220b6adf2d3676dccc23b54
Andrei Zhyunou <jivnov27@gmail.com>
2021-04-19 13:55:21 +0200

    Add MongoDB support

--- server/index.js
+++ server/index.js
@@ -3,3 +3,19 @@ import bodyParser from "body-parser";
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postsRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use('/post', postsRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors());
+
+ //TODO: change mongodb host
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log("DB start"))
+    .catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify", false)
